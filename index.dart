0cccbf09feb31cbd6a8fa059ff11e527ad98f54d
HiteshCode-Projects <hiteshtokasiya@gmail.com>
2026-03-10 20:27:30 +0530

    variable

---
+++ index.dart
@@ -0,0 +1,40 @@
+void main() {
+  print("Hello Guys");
+
+  //Data Handling
+  //1. Variables - IS LIKE A BOX - It is helping us to store data
+  //String - "Anything in double qutoes" or 'Text'
+  String name = "Pranav";
+  print(name);
+
+  //int - Whole Number
+  int age = 20;
+  print(age);
+
+  //double - Decimal Number
+  double price = 99.99;
+  print(price);
+
+  //bool - true or false
+  bool isLoggedin = true;
+  print(isLoggedin);
+
+  //var keyword (Smart Type)
+  var username = "Swadhin";
+  var passsword = 12345;
+  var rating = 4.5;
+
+  print(username);
+  print(passsword);
+
+  //dynamic (Can Change type)
+  dynamic data = "Udayshree";
+  data = 12;
+  data = true;
+  print(data);
+
+  //const - Fixed Forever
+  const appName = "MyApp";
+
+  print(appName);
+}
